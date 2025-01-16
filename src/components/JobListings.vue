cdf8bd59e1baadc0ccd429702b176935c5374d2a
Dorstol <olehsofronov@gmail.com>
2025-01-16 17:12:31 +0200

    jobs components

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,8 +1,31 @@
 <script setup>
+import jobsData from '@/jobs.json';
+import JobCard from '@/components/JobCard.vue';
+import { ref } from 'vue';
+
+const jobs = ref(jobsData);
+
 
 </script>
 
 
 <template>
-    Job Listings
+    <section class="bg-green-50 px-4 py-10">
+      <div class="container-xl lg:container m-auto">
+        <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+          Browse Jobs
+        </h2>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <JobCard v-for="job in jobs" :key="job.id" :job="job"/>
+        </div>
+      </div>
+    </section>
+
+    <section class="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
 </template>
