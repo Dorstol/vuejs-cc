8aa17df88d7608434bd6c87755cfe2bc3db1ff93
Dorstol <olehsofronov@gmail.com>
2025-01-23 16:46:43 +0000

    update JobView

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,10 +1,14 @@
 <script setup>
 import JobCard from '@/components/JobCard.vue';
-import { defineProps, ref, onMounted } from 'vue';
+import { defineProps, reactive, onMounted } from 'vue';
 import { RouterLink } from 'vue-router';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 import axios from 'axios';
 
-const jobs = ref([]);
+const state = reactive({
+  jobs: [],
+  isLoading: true,
+});
 
 defineProps({
   limit: Number,
@@ -17,9 +21,11 @@ defineProps({
 onMounted(async () => {
   try {
     const response = await axios.get('http://localhost:5000/jobs');
-    jobs.value = response.data;
+    state.jobs = response.data;
   } catch (error) {
     console.log("Error fetching", error);
+  } finally {
+    state.isLoading = false;
   }
 });
 </script>
@@ -31,8 +37,13 @@ onMounted(async () => {
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobCard v-for="job in jobs.slice(0, limit || jobs.lenght)" :key="job.id" :job="job" />
+
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
+
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobCard v-for="job in state.jobs.slice(0, limit || state.jobs.lenght)" :key="job.id" :job="job" />
       </div>
     </div>
   </section>
